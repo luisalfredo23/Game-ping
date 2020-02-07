fef0ac0312a7e6192ceff9e2e77fd8e4703491c8
luisalfredo23 <60792896+luisalfredo23@users.noreply.github.com>
2020-02-07 12:32:58 -0600

    primera versión del videojuego
    Es la primera parte de mi videojuego, sobre la estructura

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(450, 450);
+}
+
+function draw() {
+  background(220);
+  strokeJoin(150);
+  strokeCap(150);
+  rect(50,150,2,50);
+  fill(255,10,10);
+  line(200,150,200,175);
+  ellipse(200,150);
+  strokeWeight(4);
+  rect(350,150,2,50);
+  fill(255,10,10);
+  
+}
